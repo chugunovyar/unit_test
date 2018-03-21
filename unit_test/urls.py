1b87ba749becbb6700fdb7e9935e9991e62d6c14
Chugunov Yaroslav <chugunovyar@yandex.ru>
2018-03-21 23:01:47 +0300

    Добавил Main app

--- unit_test/urls.py
+++ unit_test/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from api import urls as api_urls
+from main.views import Main
 
 urlpatterns = [
+    url(r'^$', Main.as_view()),
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(api_urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
