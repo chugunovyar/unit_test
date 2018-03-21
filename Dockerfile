becfcf067a12e47ed297fb2e9d9e6c957ad05b23
Chugunov Yarsolav <Ya.Chugunov@magicgold.ru>
2018-03-21 12:19:06 +0300

    First commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3
+ENV PYTHONUNBUFFERED 1
+ENV http_proxy http://169.154.0.13:3128/
+ENV https_proxy https://169.154.0.13:3128/
+RUN mkdir /code
+WORKDIR /code
+ADD requirements.txt /code/
+RUN pip install -r requirements.txt
+ADD . /code/
---
+++ asynctasks/celeryconf.py
@@ -0,0 +1,22 @@
+import os
+from kombu import Queue
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "unit_test.settings")
+
+app = Celery('datanrm', backend='amqp', \
+             broker='amqp://' + user + ':' + password + '@'+ host +':'+ port +'//')
+
+app.conf.task_default_queues = 'default'
+
+app.conf.task_queues = (
+    Queue('default', routing_key='asynctasks.#'),
+)
+
+app.conf.task_routes = {
+    'asynctasks.tasks.*':{'queue':'default'},
+}
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
